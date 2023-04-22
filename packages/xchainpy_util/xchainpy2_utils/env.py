0276e67efdb50db91df26355ba3b9ceb4d9ad179
Tirinox <developer@tirinox.ru>
2023-04-22 20:42:11 +0300

    Util: added some THORNode urls

---
+++ packages/xchainpy_util/xchainpy2_utils/env.py
@@ -0,0 +1,5 @@
+# Pre-hard-fork endpoints (blocks 1 through 4786559):
+THORNODE_9R_ARCHIVE_V0 = 'thornode-v0.ninerealms.com'
+
+# Post-hard-fork endpoints (block 4786560 to present):
+THORNODE_9R_CURRENT = 'thornode-v1.ninerealms.com'
