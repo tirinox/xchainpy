59ed0ce2778129a8655e65e168df851c0bd3d084
Tirinox <developer@tirinox.ru>
2023-04-28 20:46:45 +0300

    Consts: TC addresses + U/A

---
+++ packages/xchainpy_thorchain_query/xchainpy2_thorchain_query/const.py
@@ -0,0 +1,6 @@
+DEFAULT_USER_AGENT = 'XChainPy2/0.0.1/python'
+
+TC_RESERVE_ADDR = 'thor1dheycdevq39qlkxs2a6wuuzyn4aqxhve4qxtxt'
+TC_BOND_ADDR = 'thor17gw75axcnr8747pkanye45pnrwk7p9c3cqncsv'
+TC_POOL_ADDR = 'thor1g98cy3n9mmjrpn0sxmn63lztelera37n8n67c0'
+TC_STANDBY_RESERVE_ADDR = 'thor1lj62pg6ryxv2htekqx04nv7wd3g98qf9gfvamy'
