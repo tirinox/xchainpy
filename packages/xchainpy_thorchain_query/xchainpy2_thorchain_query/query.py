9b068d5121a0e9d0b817a367fc7d63f32d13090a
Tirinox <developer@tirinox.ru>
2023-05-18 17:31:09 +0300

    TCQuery: calc_swap_estimate

--- packages/xchainpy_thorchain_query/xchainpy2_thorchain_query/query.py
+++ packages/xchainpy_thorchain_query/xchainpy2_thorchain_query/query.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
+from decimal import Decimal
 
-from xchainpy2_utils import DEFAULT_CHAIN_ATTRS, CryptoAmount, Asset, Address, RUNE_DECIMAL, Amount, AssetBTC
-from xchainpy2_utils.swap import get_base_amount_with_diff_decimals
+from xchainpy2_utils import DEFAULT_CHAIN_ATTRS, CryptoAmount, Asset, Address, RUNE_DECIMAL, Amount, AssetBTC, \
+    MAX_BASIS_POINTS
+from xchainpy2_utils.swap import get_base_amount_with_diff_decimals, calc_network_fee, calc_outbound_fee, \
+    is_gas_asset
 from .cache import THORChainCache
 from .const import DEFAULT_INTERFACE_ID
-from .models import TxDetails, SwapEstimate
+from .models import TxDetails, SwapEstimate, SwapOutput, TotalFees
 
 
 class THORChainQuery:
@@ -103,7 +106,7 @@ class THORChainQuery:
 
             tx_details.tx_estimate.wait_time_seconds = outbound_delay + inbound_delay
 
-            limit = Amount.from_base(lim_asset_amount_8.amount, 8)
+            limit = Amount.from_base(lim_asset_amount_8, 8)
             tx_details.memo = self.construct_swap_memo(
                 input_coin,
                 destination_asset,
@@ -139,10 +142,27 @@ class THORChainQuery:
                                  f"must have decimals of {self.native_decimal}")
 
         pool = await self.cache.get_pool_for_asset(input_coin.asset)  # todo!
-        # todo...
+        native_decimals = pool.thornode_details.decimals
+        if native_decimals and native_decimals != '-1' and input_coin.amount.decimals != native_decimals:
+            raise ValueError(f"input asset "
+                             f"{input_coin.asset!s} "
+                             f"must have decimals of {native_decimals}")
 
         if input_coin.asset == destination_asset:
-            raise ValueError("input_coin.asset and destination_asset cannot be the same")
+            raise ValueError(f"input_coin.asset and destination_asset cannot be the same ({destination_asset})")
+
+        if input_coin.amount.internal_amount <= 0:
+            raise ValueError(f"input_coin.amount cannot be negative ({input_coin.amount})")
+
+        # Affiliate fee % can't exceed 10% because this is set by TC.
+        if affiliate_fee_basis_points > 1000:
+            raise ValueError(f"affiliate_fee_basis_points cannot exceed 1000 ({affiliate_fee_basis_points})")
+
+        if affiliate_fee_basis_points < 0:
+            raise ValueError(f"affiliate_fee_basis_points cannot be negative ({affiliate_fee_basis_points})")
+
+        if slip_limit < 0.0 or slip_limit > 1.0:
+            raise ValueError(f"slip_limit must be between 0.0 and 1.0 ({slip_limit})")
 
         return True
 
@@ -156,7 +176,89 @@ class THORChainQuery:
                                  interface_id,
                                  source_inbound_details,
                                  destination_inbound_details) -> SwapEstimate:
-        pass
+        # If input is already in 8 decimals skip the convert
+        if input_coin.amount.decimals != RUNE_DECIMAL:
+            input_coin = await self.cache.convert(input_coin, input_coin.asset)
+
+        inbound_fee_in_inbound_gas_asset = calc_network_fee(input_coin.asset, source_inbound_details)
+        outbound_fee_in_outbound_gas_asset = calc_outbound_fee(destination_asset, destination_inbound_details)
+
+        # Check outbound fee is equal too or greater than 1 USD * need to find a more permanent solution to this.
+        # referencing just 1 stable coin pool has problems
+        if destination_asset.chain != self.cache.chain and not destination_asset.synth:
+            deepest_usd_pool = await self.cache.get_deepest_usd_pool()
+            usd_asset = deepest_usd_pool.asset
+            usd_min_fee = CryptoAmount(
+                Amount.from_asset("1", deepest_usd_pool.thornode_details.decimals), usd_asset
+            )
+            outbound_fee = await self.cache.convert(outbound_fee_in_outbound_gas_asset, usd_asset)
+            if outbound_fee.amount.internal_amount < usd_min_fee.amount.internal_amount:
+                # fixme: why native asset (RUNE)?
+                outbound_fee_in_outbound_gas_asset = await self.cache.convert(usd_min_fee, self.cache.native_asset)
+
+        # --- Remove Fees from inbound before doing the swap ---
+
+        inbound_fee_in_inbound_gas_asset = await self.cache.convert(inbound_fee_in_inbound_gas_asset, input_coin.asset)
+        # if it is a gas asset, take away inbound fee, else leave it as is.
+        # Still allow inboundFeeInInboundGasAsset to pass to swapEstimate.totalFees.inboundFee so user is aware
+        # if the gas requirements.
+        if is_gas_asset(input_coin.asset):
+            input_minus_outbound_fee_in_asset = input_coin - inbound_fee_in_inbound_gas_asset
+        else:
+            input_minus_outbound_fee_in_asset = input_coin
+
+        # remove any affiliateFee. netInput * affiliateFee (percentage) of the destination asset type
+        if affiliate_fee_basis_points:
+            affiliate_fee_percent = affiliate_fee_basis_points / MAX_BASIS_POINTS
+        else:
+            affiliate_fee_percent = 0
+
+        affiliate_fee_in_asset = input_minus_outbound_fee_in_asset * affiliate_fee_percent
+
+        if affiliate_fee_in_asset.asset == self.cache.native_asset:  # rune/cacao
+            affiliate_fee_swap_out_in_rune = SwapOutput(
+                output=affiliate_fee_in_asset,
+                swap_fee=CryptoAmount(Amount(0, self.native_decimal), self.cache.native_asset),
+                slip=Decimal(0),
+            )
+        else:
+            affiliate_fee_swap_out_in_rune = await self.cache.get_expected_swap_output(
+                affiliate_fee_in_asset,
+                self.cache.native_asset
+            )
+
+        # remove the affiliate fee from the input.
+        input_net_in_asset = input_minus_outbound_fee_in_asset - affiliate_fee_in_asset
+
+        # Now calculate swap output based on inputNetAmount
+        swap_out_in_dest_asset = await self.cache.get_expected_swap_output(
+            input_net_in_asset,
+            destination_asset
+        )
+        # // ---------------- Remove Outbound Fee ---------------------- /
+
+        outbound_fee_in_dest_asset = await self.cache.convert(
+            outbound_fee_in_outbound_gas_asset, destination_asset
+        )
+        net_output_in_asset = swap_out_in_dest_asset.output - outbound_fee_in_dest_asset
+
+        total_fees = TotalFees(
+            inbound_fee=inbound_fee_in_inbound_gas_asset,
+            swap_fee=swap_out_in_dest_asset.swap_fee,
+            outbound_fee=outbound_fee_in_outbound_gas_asset,
+            affiliate_fee=affiliate_fee_swap_out_in_rune.swap_fee,
+        )
+
+        swap_estimate = SwapEstimate(
+            total_fees,
+            swap_out_in_dest_asset.slip,
+            net_output_in_asset,
+            0,
+            can_swap=False,  # will be set within EstimateSwap if canSwap = true
+            errors=[]  # assume false for now, the getSwapEstimateErrors() step will flip this flag if required
+        )
+
+        return swap_estimate
 
     async def get_swap_estimate_errors(self,
                                        input_coin: CryptoAmount,
