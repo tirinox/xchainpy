ac097e4d0df3c5a7454081458b30b10f8c725cf4
Tirinox <developer@tirinox.ru>
2023-05-28 12:07:14 +0300

    Client: unwind constructor parameters

--- packages/xchainpy_client/xchainpy2_client/base_client.py
+++ packages/xchainpy_client/xchainpy2_client/base_client.py
@@ -2,34 +2,44 @@ import abc
 from typing import Optional, List
 
 from packages.xchainpy_client.xchainpy2_client.models import TxParams, XcTx, Fees, TxHistoryParams, TxPage, \
-    XChainClientParams, FeeBounds, Fee
+    FeeBounds, Fee, RootDerivationPaths
 from xchainpy2_crypto import validate_mnemonic
-from xchainpy2_utils import Address, Asset, CryptoAmount, Chain, NetworkType
+from xchainpy2_utils import Address, CryptoAmount, Chain, NetworkType
 
 INF_FEE = Fee(1_000_000_000_000_000_000)
 
 
 class XChainClient(abc.ABC):
-    def __init__(self, chain: Chain, params: XChainClientParams):
+    def __init__(self,
+                 chain: Chain,
+                 network: Optional[NetworkType] = None,
+                 phrase: Optional[str] = None,
+                 fee_bound: FeeBounds = FeeBounds(lower=Fee(0), upper=INF_FEE),
+                 root_derivation_paths: Optional[RootDerivationPaths] = None,
+                 ):
         """
         Client has to be initialised with network type and phrase.
         It will throw an error if an invalid phrase has been passed.
-        :param chain: chain type
-        :param params: XChainClientParams
+
+        :param chain: Chain name (see utils/chain.py)
+        :param network: Network type (see utils/network_type.py)
+        :param phrase: Mnemonic phrase
+        :param fee_bound: Fee bounds
+        :param root_derivation_paths: Root derivation paths for private key for each Network type
         """
         self.chain = chain
 
-        self.fee_bound = params.fee_bound or FeeBounds(lower=Fee(0), upper=INF_FEE)
-        self.root_derivation_paths = params.root_derivation_paths
+        self.fee_bound = fee_bound
+        self.root_derivation_paths = root_derivation_paths
 
         self.network = NetworkType.MAINNET
-        self.set_network(params.network)
+        self.set_network(network)
 
         # NOTE: we don't call this.setPhrase() to void generating an address and paying the perf penalty
-        if params.phrase:
-            if not validate_mnemonic(params.phrase):
+        if phrase:
+            if not validate_mnemonic(phrase):
                 raise Exception('Invalid phrase')
-            self.phrase = params.phrase
+            self.phrase = phrase
         else:
             self.phrase = None
 
