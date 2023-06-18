03def590868328a7b5f3e890b22a8ca0edf48b6b
Tirinox <developer@tirinox.ru>
2023-06-18 22:18:32 +0400

    Cosmos: demo example

--- examples/cosmos_demo.py
+++ examples/cosmos_demo.py
@@ -4,8 +4,14 @@ from xchainpy2_cosmos import CosmosGaiaClient
 
 
 async def demo_read_txs():
-    client = CosmosGaiaClient()
-    txs = await client.search_tx(message_sender='cosmos1f9ta55rme2mwy9nwugzp5r9rl9h54a70yf8nfz')
+    demo_addy = 'cosmos1f9ta55rme2mwy9nwugzp5r9rl9h54a70yf8nfz'
+
+    client = CosmosGaiaClient(client_urls='https://api.cosmos.network')
+
+    balance = await client.get_balance(demo_addy)
+    print(balance)
+
+    txs = await client.search_tx(message_sender=demo_addy)
     print('txs', txs)
 
 
