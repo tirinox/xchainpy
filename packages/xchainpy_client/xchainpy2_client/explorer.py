30ad808ed9d9040cec3472b73a05af7e554cfb4e
Tirinox <developer@tirinox.ru>
2023-07-14 22:20:06 +0300

    Client: fix explorer provider

--- packages/xchainpy_client/xchainpy2_client/explorer.py
+++ packages/xchainpy_client/xchainpy2_client/explorer.py
@@ -7,7 +7,7 @@ class ExplorerProvider(NamedTuple):
     explorer_tx_url: str
 
     def get_address_url(self, address: str) -> str:
-        return self.explorer_tx_url.format(address=address)
+        return self.explorer_address_url.format(address=address)
 
     def get_tx_url(self, tx_id: str) -> str:
         return self.explorer_tx_url.format(tx_id=tx_id)
