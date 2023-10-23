e9e5e2e41305bf1beac0ffec9f0c06f26a23dd5a
Tirinox <developer@tirinox.ru>
2023-10-23 21:05:55 +0300

    TcAMM: memo parse

---
+++ packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/consts.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from xchainpy2_utils import RUNE_DECIMAL
+
+RUNE_IDEAL_SUPPLY = 500_000_000
+RUNE_SUPPLY_AFTER_SWITCH = 486_051_059
+THOR_BASIS_POINT_MAX = 10_000
+THOR_DIVIDER = float(10 ** RUNE_DECIMAL)
+THOR_DIVIDER_INV = 1.0 / THOR_DIVIDER
+
+
+class TxType(Enum):
+    ADD_LIQUIDITY = 'addLiquidity'
+    SWAP = 'swap'
+    WITHDRAW = 'withdraw'
+    DONATE = 'donate'
+    REFUND = 'refund'
+
+    LOAN_OPEN = 'loan+'
+    LOAN_CLOSE = 'loan-'
+
+    LIMIT_ORDER = 'limit_order'
+    BOND = 'bond'
+    UNBOND = 'unbond'
+    LEAVE = 'leave'
+    THORNAME = 'thorname'
+    OUTBOUND = 'out'
