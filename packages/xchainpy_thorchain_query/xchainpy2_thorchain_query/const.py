8fbf81a92409927c6c6138813ac57b5c8fcf8e54
Tirinox <developer@tirinox.ru>
2023-11-19 12:45:08 +0300

    Maya fee = 1 cacao

--- packages/xchainpy_thorchain_query/xchainpy2_thorchain_query/const.py
+++ packages/xchainpy_thorchain_query/xchainpy2_thorchain_query/const.py
@@ -33,6 +33,6 @@ ETH_DECIMALS = 18
 AVAX_DECIMALS = 18
 
 RUNE_NETWORK_FEE = CryptoAmount(Amount.from_asset(Decimal("0.02"), RUNE_DECIMAL), AssetRUNE)
-CACAO_NETWORK_FEE = CryptoAmount(Amount.from_asset(Decimal("0.02"), CACAO_DECIMAL), AssetCACAO)
+CACAO_NETWORK_FEE = CryptoAmount(Amount.from_asset(Decimal("1"), CACAO_DECIMAL), AssetCACAO)
 
 DEFAULT_EXTRA_ADD_MINUTES = 15
