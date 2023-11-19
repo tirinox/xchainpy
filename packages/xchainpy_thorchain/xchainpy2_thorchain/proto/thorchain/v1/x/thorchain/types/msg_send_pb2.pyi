23a368076bf8d0350f334baaa05573861f05a880
Tirinox <developer@tirinox.ru>
2023-11-19 12:39:33 +0300

    TC: cosmos coin

---
+++ packages/xchainpy_thorchain/xchainpy2_thorchain/proto/thorchain/v1/x/thorchain/types/msg_send_pb2.pyi
@@ -0,0 +1,18 @@
+from cosmos.base.v1beta1 import coin_pb2 as _coin_pb2
+from gogoproto import gogo_pb2 as _gogo_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class MsgSend(_message.Message):
+    __slots__ = ["from_address", "to_address", "amount"]
+    FROM_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    TO_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    from_address: bytes
+    to_address: bytes
+    amount: _containers.RepeatedCompositeFieldContainer[_coin_pb2.Coin]
+    def __init__(self, from_address: _Optional[bytes] = ..., to_address: _Optional[bytes] = ..., amount: _Optional[_Iterable[_Union[_coin_pb2.Coin, _Mapping]]] = ...) -> None: ...
