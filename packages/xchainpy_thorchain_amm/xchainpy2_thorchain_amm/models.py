4f7c74124a886a5a1b41f9c676093c82f064e3e3
Tirinox <developer@tirinox.ru>
2023-12-17 20:52:01 +0300

    AMM: wallet direct params

--- packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/models.py
+++ packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/models.py
@@ -1,34 +1,10 @@
 from datetime import datetime
-from typing import NamedTuple, Optional, List, Set, Dict
+from typing import NamedTuple, Optional, List, Dict
 
 from xchainpy2_client import FeeOption
-from xchainpy2_thorchain_query import THORChainQuery
 from xchainpy2_utils import Chain, CryptoAmount, Asset
 
 ALL = None
-
-
-class WalletSettings(NamedTuple):
-    phrase: str
-    enabled_chains: Set[Chain]
-    query_api: Optional[THORChainQuery] = None
-    concurrency: int = 5
-
-    @classmethod
-    def default(cls, phrase, enabled_chains=ALL):
-        return cls(
-            phrase=phrase,
-            enabled_chains={
-                Chain.THORChain,
-                Chain.Maya,
-                Chain.Cosmos,
-                Chain.Binance,
-                Chain.Cosmos,
-            } if enabled_chains is ALL else enabled_chains
-        )
-
-    def is_enabled(self, chain: Chain):
-        return chain in self.enabled_chains
 
 
 class ChainBalances(NamedTuple):
--- packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/wallet.py
+++ packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/wallet.py
@@ -1,6 +1,6 @@
 import asyncio
 import datetime
-from typing import Optional
+from typing import Optional, Set
 
 from xchainpy2_client import NoClient, XChainClient
 from xchainpy2_thorchain_query.models import InboundDetail
@@ -32,26 +32,40 @@ except ImportError:
 
 from xchainpy2_thorchain_query import THORChainQuery
 from xchainpy2_utils import Chain, EVM_CHAINS
-from xchainpy2_thorchain_amm import WalletSettings, AllBalances, ChainBalances
+from .models import AllBalances, ChainBalances, ALL
 from .evm_helper import EVMHelper
 
 
 class Wallet:
-    def __init__(self, config: WalletSettings):
-        self._semaphore = asyncio.Semaphore(config.concurrency)
+    CLIENT_CLASSES = {
+        Chain.THORChain: THORChainClient,
+        Chain.Cosmos: CosmosGaiaClient,
+        Chain.Binance: BinanceChainClient,
+        Chain.Maya: MayaChainClient,
+        Chain.Bitcoin: BitcoinClient,
+        # to be continued
+    }
 
-        if not config.query_api:
+    def __init__(self, phrase: str,
+                 query_api: Optional[THORChainQuery] = None,
+                 enabled_chains: Set[Chain] = ALL,
+                 concurrency: int = 5):
+        self._semaphore = asyncio.Semaphore(concurrency)
+
+        if not query_api:
             self.query_api = THORChainQuery()
         else:
-            self.query_api = config.query_api
+            self.query_api = query_api
+
+        self.enabled_chains = enabled_chains
 
         self.network = self.cache.network
 
         self.clients = {}
-        self._create_clients(config)
+        self._create_clients(phrase)
 
         self._evm_helpers = {}
-        self._init_evm_helpers(config)
+        self._init_evm_helpers()
 
     @property
     def cache(self):
@@ -60,27 +74,24 @@ class Wallet:
     def get_client(self, chain: Chain) -> Optional[XChainClient]:
         return self.clients.get(chain)
 
-    def _init_evm_helpers(self, config: WalletSettings):
+    def is_chain_enabled(self, chain: Chain):
+        if not self.enabled_chains:
+            return True
+        return chain in self.enabled_chains
+
+    def _init_evm_helpers(self):
         for chain in EVM_CHAINS:
-            if config.is_enabled(chain):
+            if self.is_chain_enabled(chain):
                 self._evm_helpers[chain] = EVMHelper(self.get_client(chain), self.cache)
 
-    def _create_clients(self, config: WalletSettings):
-        client_classes = {
-            Chain.THORChain: THORChainClient,
-            Chain.Cosmos: CosmosGaiaClient,
-            Chain.Binance: BinanceChainClient,
-            Chain.Maya: MayaChainClient,
-            Chain.Bitcoin: BitcoinClient,
-            # to be continued
-        }
-        for chain, chain_class in client_classes.items():
-            if config.is_enabled(chain):
+    def _create_clients(self, phrase):
+        for chain, chain_class in self.CLIENT_CLASSES.items():
+            if self.is_chain_enabled(chain):
                 if issubclass(chain_class, NoClient):
                     raise ImportError(f"{chain} client is not found. Try to install it by running"
                                       f" 'pip install xchainpy2_{chain}' or remove it from the enabled chains set in "
                                       f"config(WalletSettings).")
-                self.clients[chain] = chain_class(phrase=config.phrase)
+                self.clients[chain] = chain_class(phrase=phrase)
 
     async def get_all_balances(self) -> AllBalances:
         result = AllBalances(
