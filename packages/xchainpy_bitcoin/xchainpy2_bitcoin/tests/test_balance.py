b1123fc593aa5ae1be256e52ec3f8e0b50dbb9ee
Tirinox <developer@tirinox.ru>
2023-12-30 09:22:23 +0300

    Bitcoin: more test balance

--- packages/xchainpy_bitcoin/xchainpy2_bitcoin/tests/test_balance.py
+++ packages/xchainpy_bitcoin/xchainpy2_bitcoin/tests/test_balance.py
@@ -1,5 +1,6 @@
 import pytest
 import requests_mock
+from bitcoinlib.services.services import ServiceError
 
 from xchainpy2_bitcoin import BitcoinClient
 from xchainpy2_bitcoin.tests.common import load_json
@@ -19,3 +20,21 @@ async def test_balance():
         assert balances
         assert balances[0].asset == AssetBTC
         assert balances[0].amount.as_base.internal_amount == 5589017
+
+        m.get('https://mempool.space/api/address/bc1pjt9a2j85q8yn6p0g7q07c38nev9wct9fquj9fda7283y94khmc0s3fwp4g',
+              text='Invalid Bitcoin address')
+        m.get('https://blockstream.info/api/address/bc1pjt9a2j85q8yn6p0g7q07c38nev9wct9fquj9fda7283y94khmc0s3fwp4g',
+              text='Invalid Bitcoin address')
+
+        with pytest.raises(ServiceError):
+            await btc.get_balance('bc1pjt9a2j85q8yn6p0g7q07c38nev9wct9fquj9fda7283y94khmc0s3fwp4g')
+
+        m.get('https://mempool.space/api/address/bc1qlejn5eh6kf6wm7mxv2drnt0mk66uthvxzcemvc',
+              json=load_json('empty_balance'))
+        m.get('https://blockstream.info/api/address/bc1qlejn5eh6kf6wm7mxv2drnt0mk66uthvxzcemvc',
+                json=load_json('empty_balance'))
+
+        balances = await btc.get_balance('bc1qlejn5eh6kf6wm7mxv2drnt0mk66uthvxzcemvc')
+        assert balances
+        assert balances[0].asset == AssetBTC
+        assert balances[0].amount.as_base.internal_amount == 0
