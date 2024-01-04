fdeb9c7c9ede79f8c024c13bba29656cae91b0e5
Tirinox <developer@tirinox.ru>
2024-01-04 23:11:54 +0300

    Maya: M-NFT memo builder

--- packages/xchainpy_mayachain/xchainpy2_mayachain/mrc20/memo.py
+++ packages/xchainpy_mayachain/xchainpy2_mayachain/mrc20/memo.py
@@ -1,24 +1,28 @@
 from xchainpy2_utils import Amount
 
 
+def check_ticker(ticker):
+    if not (1 <= len(ticker) <= 4):
+        raise ValueError('Ticker must be 1-4 characters long')
+
+    if not ticker.isalnum():
+        raise ValueError('Symbol must contain only letters and digits')
+
+
+MRC20_DECIMALS = 10
+
+
+def get_amount(amount):
+    return Amount.automatic(amount, MRC20_DECIMALS)
+
+
 class MRC20Memo:
-    DECIMALS = 10
-
-    @staticmethod
-    def check_ticker(ticker):
-        if not (1 <= len(ticker) <= 4):
-            raise ValueError('Ticker must be 1-4 characters long')
-
-    @classmethod
-    def get_amount(cls, amount):
-        return Amount.automatic(amount, cls.DECIMALS)
-
     @classmethod
     def deploy_token(cls, ticker, supply, mint_limit=None, mint_price=None) -> str:
         # "MRC-20:deploy:[ticker]:[supply]:[mint-limit]:[mint-price]"
-        cls.check_ticker(ticker)
+        check_ticker(ticker)
 
-        supply = cls.get_amount(supply)
+        supply = get_amount(supply)
         if supply.internal_amount <= 0:
             raise ValueError('Supply must be positive')
 
@@ -32,15 +36,15 @@ class MRC20Memo:
     @classmethod
     def set_mint_price(cls, ticker, mint_price) -> str:
         # "MRC-20:set-mint-price:[ticker]:[mint-price]"
-        cls.check_ticker(ticker)
-        mint_price = cls.get_amount(mint_price)
+        check_ticker(ticker)
+        mint_price = get_amount(mint_price)
         return ':'.join(['MRC-20', 'set-mint-price', ticker, mint_price])
 
     @classmethod
     def mint(cls, ticker, amount, recipient=None) -> str:
         # "MRC-20:mint:[ticker]:[amount]:[recipient]"
-        cls.check_ticker(ticker)
-        amount = cls.get_amount(amount)
+        check_ticker(ticker)
+        amount = get_amount(amount)
         if amount.internal_amount <= 0:
             raise ValueError('Amount must be positive')
 
@@ -52,8 +56,8 @@ class MRC20Memo:
     @classmethod
     def transfer(cls, ticker, amount) -> str:
         # "MRC-20:transfer:[ticker]:[amount]
-        cls.check_ticker(ticker)
-        amount = cls.get_amount(amount)
+        check_ticker(ticker)
+        amount = get_amount(amount)
         if amount.internal_amount <= 0:
             raise ValueError('Amount must be positive')
 
@@ -62,12 +66,12 @@ class MRC20Memo:
     @classmethod
     def sell(cls, ticker, amount, price) -> str:
         # "MRC-20:sell:[ticker]:[amount]:[price]"
-        cls.check_ticker(ticker)
-        amount = cls.get_amount(amount)
+        check_ticker(ticker)
+        amount = get_amount(amount)
         if amount.internal_amount <= 0:
             raise ValueError('Amount must be positive')
 
-        price = cls.get_amount(price)
+        price = get_amount(price)
         if price.internal_amount <= 0:
             raise ValueError('Price must be positive')
 
@@ -76,7 +80,7 @@ class MRC20Memo:
     @classmethod
     def cancel(cls, ticker, tx_hash) -> str:
         # "MRC-20:cancel:[ticker]:[hash]"
-        cls.check_ticker(ticker)
+        check_ticker(ticker)
         if not tx_hash:
             raise ValueError('tx_hash must not be empty')
         return ':'.join(['MRC-20', 'cancel', ticker, tx_hash])
@@ -84,8 +88,8 @@ class MRC20Memo:
     @classmethod
     def buy(cls, ticker, amount, tx_hash) -> str:
         # "MRC-20:buy:[ticker]:[amount]:[hash]"
-        cls.check_ticker(ticker)
-        amount = cls.get_amount(amount)
+        check_ticker(ticker)
+        amount = get_amount(amount)
         if amount.internal_amount <= 0:
             raise ValueError('Amount must be positive')
 
@@ -97,9 +101,88 @@ class MRC20Memo:
     @classmethod
     def burn(cls, ticker, amount) -> str:
         # "MRC-20:burn:[ticker]:[amount]"
-        cls.check_ticker(ticker)
-        amount = cls.get_amount(amount)
+        check_ticker(ticker)
+        amount = get_amount(amount)
         if amount.internal_amount <= 0:
             raise ValueError('Amount must be positive')
 
         return ':'.join(['MRC-20', 'burn', ticker, amount])
+
+
+class MNFTMemo:
+
+    @staticmethod
+    def yes_no(value):
+        return 'YES' if value else 'NO'
+
+    @classmethod
+    def deploy_token(cls, name, symbol, supply, base_url, pre_minted,
+                     minted_by_owner_only=None, mint_price=None) -> str:
+        # "M-NFT:deploy:[name]:[symbol]:[supply]:[baseURL]:[preMinted]:[mintedByOwnerOnly]:[mintPrice]"
+
+        check_ticker(symbol)
+
+        symbol = symbol.upper()
+
+        if not (1 <= len(name) <= 20):
+            raise ValueError('Name must be 1-20 characters long')
+
+        supply = int(supply)
+        if not (1 <= supply <= 100000):
+            raise ValueError('Supply must be between 1 and 100000')
+
+        components = ['M-NFT', 'deploy', name, symbol, supply, base_url, cls.yes_no(pre_minted)]
+
+        if minted_by_owner_only is not None:
+            components.append(cls.yes_no(minted_by_owner_only))
+        if mint_price is not None:
+            components.append(mint_price)
+
+        return ':'.join(components)
+
+    @classmethod
+    def update_mint_price(cls, symbol, mint_price) -> str:
+        # "M-NFT:update-mint-price:[symbol]:[mintPrice]"
+        check_ticker(symbol)
+        mint_price = get_amount(mint_price)
+        return ':'.join(['M-NFT', 'update-mint-price', symbol, mint_price])
+
+    @classmethod
+    def set_base_url(cls, symbol, base_url) -> str:
+        # "M-NFT:set-base-url:[symbol]:[baseURL]"
+        check_ticker(symbol)
+        return ':'.join(['M-NFT', 'set-base-url', symbol, base_url])
+
+    @classmethod
+    def mint(cls, symbol, token_id=None) -> str:
+        # "M-NFT:mint:[symbol]:[id]"
+        check_ticker(symbol)
+        components = ['M-NFT', 'mint', symbol]
+        if token_id is not None:
+            components.append(token_id)
+        return ':'.join(components)
+
+    @classmethod
+    def transfer(cls, symbol, token_id) -> str:
+        # "M-NFT:transfer:[symbol]:[id]"
+        check_ticker(symbol)
+        return ':'.join(['M-NFT', 'transfer', symbol, token_id])
+
+    @classmethod
+    def sell(cls, symbol, token_id, price) -> str:
+        # "M-NFT:sell:[symbol]:[id]:[price]"
+        check_ticker(symbol)
+        price = get_amount(price)
+        return ':'.join(['M-NFT', 'sell', symbol, token_id, price])
+
+    @classmethod
+    def cancel(cls, symbol, token_id) -> str:
+        # "M-NFT:cancel:[symbol]:[id]"
+        check_ticker(symbol)
+        return ':'.join(['M-NFT', 'cancel', symbol, token_id])
+
+    @classmethod
+    def buy(cls, symbol, token_id) -> str:
+        # "M-NFT:buy:[symbol]:[id]"
+        check_ticker(symbol)
+        return ':'.join(['M-NFT', 'buy', symbol, token_id])
