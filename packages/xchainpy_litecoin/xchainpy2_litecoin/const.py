c1b8b1fb558976f715e5a30cf9f1e808eb03fa1c
Tirinox <developer@tirinox.ru>
2024-03-02 19:41:27 +0900

    LTC: explorer urls

--- packages/xchainpy_litecoin/xchainpy2_litecoin/const.py
+++ packages/xchainpy_litecoin/xchainpy2_litecoin/const.py
@@ -1,7 +1,7 @@
+from xchainpy2_client import ExplorerProvider
 from xchainpy2_utils import NetworkType
 
 LTC_DECIMAL = 8
-
 
 ROOT_DERIVATION_PATHS = {
     NetworkType.MAINNET: "84'/2'/0'/0/",
@@ -14,8 +14,26 @@ MIN_TX_FEE = 1000
 LOWER_FEE_BOUND = 0.5
 UPPER_FEE_BOUND = 500
 
-LTC_EXPLORER = {
-    NetworkType.MAINNET: {
-    }
+LTC_BLOCKCHAIR_EXPLORER = ExplorerProvider(
+    'https://blockchair.com/litecoin/',
+    'https://blockchair.com/litecoin/address/{address}',
+    'https://blockchair.com/litecoin/transaction/{tx_id}',
+)
+
+LTC_BLOCKCHIAR_TESTNET_EXPLORER = ExplorerProvider(
+    'https://blockexplorer.one/litecoin/testnet/',
+    'https://blockexplorer.one/litecoin/testnet/address/{address}',
+    'https://blockexplorer.one/litecoin/testnet/transaction/{tx_id}',
+)
+
+BLOCKSTREAM_EXPLORERS = {
+    NetworkType.MAINNET: LTC_BLOCKCHAIR_EXPLORER,
+    NetworkType.STAGENET: LTC_BLOCKCHAIR_EXPLORER,
+    NetworkType.TESTNET: LTC_BLOCKCHIAR_TESTNET_EXPLORER,
 }
- 
+
+DEFAULT_LTC_EXPLORER = LTC_BLOCKCHAIR_EXPLORER
+
+MAX_MEMO_LENGTH = 80
+
+DEFAULT_PROVIDER_NAMES = ['blockcypher', 'blockcypher']
