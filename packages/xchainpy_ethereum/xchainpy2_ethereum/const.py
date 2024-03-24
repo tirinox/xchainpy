eb2b23db49d21eb67f1f7369891e9c2c30a0358f
Tirinox <develoloper@tirinox.ru>
2024-03-24 21:17:21 +0300

    ETH: main consts, client methods

---
+++ packages/xchainpy_ethereum/xchainpy2_ethereum/const.py
@@ -0,0 +1,28 @@
+from xchainpy2_client import ExplorerProvider
+from xchainpy2_utils import NetworkType
+
+ETH_DECIMAL = 18
+
+MAIN_NET_ETHERSCAN_PROVIDER = ExplorerProvider(
+        'https://etherscan.io',
+        'https://etherscan.io/address/{address}',
+        'https://etherscan.io/tx/{tx_id}',
+    )
+
+DEFAULT_ETH_EXPLORER_PROVIDERS = {
+    NetworkType.MAINNET: MAIN_NET_ETHERSCAN_PROVIDER,
+    NetworkType.TESTNET: ExplorerProvider(
+        # sepolia
+        'https://sepolia.etherscan.io/',
+        'https://sepolia.etherscan.io/address/{address}',
+        'https://sepolia.etherscan.io/tx/{tx_id}',
+    ),
+    NetworkType.STAGENET: MAIN_NET_ETHERSCAN_PROVIDER,
+}
+
+
+ETH_ROOT_DERIVATION_PATHS = {
+    NetworkType.MAINNET: "m/44'/60'/0'/0/",
+    NetworkType.TESTNET: "m/44'/60'/0'/0/",
+    NetworkType.STAGENET: "m/44'/60'/0'/0/",
+}
---
+++ packages/xchainpy_ethereum/xchainpy2_ethereum/eth_client.py
@@ -0,0 +1,98 @@
+import logging
+from datetime import datetime
+from typing import Optional, List, Union
+
+from xchainpy2_client import XChainClient, RootDerivationPaths, FeeBounds, Fees, XcTx, TxPage
+from xchainpy2_ethereum import ETH_ROOT_DERIVATION_PATHS, ETH_DECIMAL, DEFAULT_ETH_EXPLORER_PROVIDERS
+from xchainpy2_utils import Chain, NetworkType, CryptoAmount, AssetETH, Asset
+
+logger = logging.getLogger(__name__)
+
+
+class EthereumClient(XChainClient):
+
+    def __init__(self,
+                 network=NetworkType.MAINNET,
+                 phrase: Optional[str] = None,
+                 private_key: Union[str, bytes, callable, None] = None,
+                 fee_bound: Optional[FeeBounds] = None,
+                 root_derivation_paths: Optional[RootDerivationPaths] = None,
+                 explorer_providers=DEFAULT_ETH_EXPLORER_PROVIDERS.copy(),
+                 wallet_index=0,
+                 ):
+        """
+        Initialize Ethereum
+        :param network: Network type. Default is `NetworkType.MAINNET`
+        :param phrase: Mnemonic phrase
+        :param private_key: Private key (if you want to use a private key instead of a mnemonic phrase)
+        :param fee_bound: Fee bound structure. See: FeeBounds
+        :param root_derivation_paths: Dictionary of derivation paths for each network type. See: ROOT_DERIVATION_PATHS
+        :param explorer_providers: Dictionary of explorer providers for each network type.
+        :param wallet_index: int (default 0)
+        """
+        root_derivation_paths = root_derivation_paths.copy() \
+            if root_derivation_paths else ETH_ROOT_DERIVATION_PATHS.copy()
+
+        super().__init__(Chain.Ethereum, network, phrase, private_key, fee_bound, root_derivation_paths, wallet_index)
+
+        self.explorers = explorer_providers
+        self._gas_asset = AssetETH
+        self._decimal = ETH_DECIMAL
+
+        self.tx_responses = {}
+        self.web3 = None
+
+    def validate_address(self, address: str) -> bool:
+        """
+        Validates a Ethereum address.
+        :param address: Address string
+        :return: True if valid, False otherwise.
+        """
+
+        if not address:
+            return False
+
+        raise NotImplementedError("Ethereum address validation is not implemented yet")
+
+    def get_address(self) -> str:
+        """
+        Get the address for the given wallet index.
+        :return: string address
+        """
+        raise NotImplementedError("Ethereum address derivation is not implemented yet")
+
+    def get_private_key(self) -> str:
+        """
+        Get the private key for the given wallet index.
+        :return:
+        """
+        raise NotImplementedError("Ethereum address derivation is not implemented yet")
+
+    async def get_balance(self, address: str = '') -> List[CryptoAmount]:
+        """
+        Get the balance of a given address.
+        :param address: By default, it will return the balance of the current wallet. (optional)
+        :return:
+        """
+        raise NotImplementedError("Ethereum balance fetching is not implemented yet")
+
+    def get_public_key(self):
+        pass
+
+    async def get_transactions(self, address: str, offset: int = 0, limit: int = 0,
+                               start_time: Optional[datetime] = None, end_time: Optional[datetime] = None,
+                               asset: Optional[Asset] = None) -> TxPage:
+        pass
+
+    async def get_transaction_data(self, tx_id: str) -> Optional[XcTx]:
+        pass
+
+    async def get_fees(self) -> Fees:
+        pass
+
+    async def transfer(self, what: CryptoAmount, recipient: str, memo: Optional[str] = None,
+                       fee_rate: Optional[int] = None, **kwargs) -> str:
+        pass
+
+    async def broadcast_tx(self, tx_hex: str) -> str:
+        pass
