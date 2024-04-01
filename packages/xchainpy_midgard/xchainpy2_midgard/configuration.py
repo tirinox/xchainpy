4b72f3309768f65ddea6f12c1edb85cb844b4a9b
Tirinox <develoloper@tirinox.ru>
2024-04-01 14:01:00 +0300

    Midgard: upgrade to v.2.20.1

--- packages/xchainpy_midgard/xchainpy2_midgard/configuration.py
+++ packages/xchainpy_midgard/xchainpy2_midgard/configuration.py
@@ -243,5 +243,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.20.1\n"\
-               "SDK Package Version: 2.18.2".\
+               "SDK Package Version: 2.20.1".\
                format(env=sys.platform, pyversion=sys.version)
