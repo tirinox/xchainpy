c9929a64c6bf3c50ca57cd8c2fe01fbaff53ba80
Tirinox <develoloper@tirinox.ru>
2024-04-30 15:02:01 +0300

    Bsc: middleware added

--- packages/xchainpy_bsc/xchainpy2_bsc/bsc_client.py
+++ packages/xchainpy_bsc/xchainpy2_bsc/bsc_client.py
@@ -1,9 +1,10 @@
 from typing import Optional, Union
 
+from web3.middleware import geth_poa_middleware
 from web3.providers import BaseProvider
 
-from xchainpy2_bsc.const import FREE_BSC_PROVIDERS, BSC_CHAIN_ID, DEFAULT_BSC_EXPLORER_PROVIDERS, BSC_SURE_FEE, \
-    BSC_NORMAL_FEE
+from xchainpy2_bsc.const import FREE_BSC_PROVIDERS, BSC_CHAIN_ID, DEFAULT_BSC_EXPLORER_PROVIDERS, BSC_NORMAL_FEE, \
+    BSC_SURE_FEE
 from xchainpy2_client import FeeBounds, RootDerivationPaths, FeeOption
 from xchainpy2_ethereum import EthereumClient, GasOptions
 from xchainpy2_ethereum.utils import select_random_free_provider
@@ -31,7 +32,12 @@ class BinanceSmartChainClient(EthereumClient):
         return select_random_free_provider(self.network, FREE_BSC_PROVIDERS)
 
     async def _deduct_gas(self, fee_option: FeeOption, gas_limit=23000) -> GasOptions:
+        # last_fee = await self.get_last_fee()
         if fee_option == FeeOption.FASTEST:
             return GasOptions.eip1559_in_gwei(BSC_SURE_FEE, 1, gas_limit)
         else:
             return GasOptions.eip1559_in_gwei(BSC_NORMAL_FEE, 1, gas_limit)
+
+    def _remake_provider(self, provider: BaseProvider):
+        super()._remake_provider(provider)
+        self.web3.middleware_onion.inject(geth_poa_middleware, layer=0)
