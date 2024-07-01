3ca2158f3ec6a3634c28cd6ee5ad137d6d60c80e
Tirinox <develoloper@tirinox.ru>
2024-07-01 13:44:36 +0300

    Eth: more docstring

--- packages/xchainpy_ethereum/xchainpy2_ethereum/utils.py
+++ packages/xchainpy_ethereum/xchainpy2_ethereum/utils.py
@@ -4,25 +4,45 @@ import os
 import re
 
 import web3
+from web3.main import BaseWeb3
 
 from xchainpy2_utils import NetworkType
 
 SELF_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MAX_APPROVAL = 2 ** 256 - 1
+"""Maximum approval value for ERC20 tokens"""
 
 
 def get_erc20_abi():
+    """
+    Loads and returns standard ERC20 token ABI as a Python dictionary
+    See: https://eips.ethereum.org/EIPS/eip-20
+
+    :return: dict
+    """
     with open(f'{SELF_DIR}/abi/erc20.json', 'r') as f:
         return json.load(f)
 
 
 def get_router_abi():
+    """
+    Loads and returns ABI of the THORChain router v4 as a Python dictionary
+    See: https://dev.thorchain.org/protocol-development/chain-clients/evm-chains.html?highlight=router#router
+
+    :return: dict
+    """
     with open(f'{SELF_DIR}/abi/router.json', 'r') as f:
         return json.load(f)
 
 
 def is_valid_eth_address(address):
+    """
+    Validates Ethereum address, including checksum validation
+
+    :param address: Ethereum address to be validated
+    :return: bool
+    """
     if not isinstance(address, str):
         return False
 
@@ -36,7 +56,16 @@ def is_valid_eth_address(address):
     return True
 
 
-def select_random_free_provider(network: NetworkType, source):
+def select_random_free_provider(network: NetworkType, source: dict) -> web3.providers.BaseProvider:
+    """
+    Selects a random free RPC provider from the source list
+
+    :param network: Type of network
+    :type network: NetworkType
+    :param source: Dictionary of free RPC providers grouped by network
+    :return: Web3 provider instance
+    :rtype web3.providers.BaseProvider
+    """
     import random
     provider_url = random.choice(source[network])
     logging.warning(f"Auto selected free RPC provider: {provider_url}. "
@@ -51,23 +80,29 @@ def select_random_free_provider(network: NetworkType, source):
 
 
 class EVMCallError(Exception):
+    """
+    This kind of exception is raised when there are issues with Web3 provider communication
+    """
     pass
 
 
-def validated_checksum_address(web3, address: str) -> str:
+def validated_checksum_address(web3_pr: BaseWeb3, address: str) -> str:
     """
     This function validates and returns a checksum address.
     In case the address is in uppercase, that means it probably came from THORChain or other similar AMM.
     it converts it to checksum address.
     Otherwise, it validates the address and returns it.
-    :param web3:
-    :param address:
-    :return:
+
+    :param web3_pr: Web3 instance
+    :type web3_pr: BaseWeb3
+    :param address: EVM Address to be validated
+    :return: Validated checksum address
+    :rtype: str
     """
     if not address:
         raise ValueError("Address is required")
     if address.isupper():  # if it is in uppercase, that means it probably came from THORChain or other similar AMM
-        address = web3.to_checksum_address(address)
+        address = web3_pr.to_checksum_address(address)
     if not is_valid_eth_address(address):
         raise ValueError(f'Invalid address: {address}')
     return address
