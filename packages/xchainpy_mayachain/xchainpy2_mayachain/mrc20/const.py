32358695106d4d799d438e9f3da87e7050c7a1ee
Tirinox <develoloper@tirinox.ru>
2024-07-06 10:54:49 +0300

    Maya: MRC20 memo and const docstrings

--- packages/xchainpy_mayachain/xchainpy2_mayachain/mrc20/const.py
+++ packages/xchainpy_mayachain/xchainpy2_mayachain/mrc20/const.py
@@ -1,31 +1,64 @@
 from xchainpy2_utils import Asset
 
 MRC20_DECIMALS = 10
+"""The number of decimals for MRC20 tokens"""
 
 MRC20_CHAIN = 'MRC20'
+"""Special Chain identifier for MRC20 assets on chain"""
+
 MNFT_CHAIN = 'MNFT'
+"""Special Chain identifier for MNFT assets on chain"""
 
 
 def is_mrc20(asset: Asset):
+    """
+    Check if the asset is a MRC20 asset
+
+    :param asset: Asset
+    :return: bool
+    """
     return asset.chain == MRC20_CHAIN
 
 
 def is_mnft(asset: Asset):
+    """
+    Check if the asset is a MNFT asset
+
+    :param asset: Asset
+    :return: bool
+    """
     return asset.chain == MNFT_CHAIN
 
 
 def make_mrc20_asset(symbol):
+    """
+    Create an Asset object from a MRC20 symbol
+
+    :param symbol: Symbol of the asset
+    :return: Asset
+    """
     return Asset(MRC20_CHAIN, symbol)
 
 
 def make_mnft_asset(symbol):
+    """
+    Create an Asset object from a MNFT symbol
+
+    :param symbol: Symbol of the asset
+    :return: Asset
+    """
     return Asset(MNFT_CHAIN, symbol)
 
 
 AssetGLD = make_mrc20_asset('GLD')
+"""Asset object for GLD"""
 
 
 class SendsType:
+    """
+    Enum for the type of MRC20 transactions
+    """
+
     SEND = 'SEND'
     STAKING = 'STAKING'
     MRC20 = 'MRC-20'
--- packages/xchainpy_mayachain/xchainpy2_mayachain/mrc20/memo.py
+++ packages/xchainpy_mayachain/xchainpy2_mayachain/mrc20/memo.py
@@ -1,7 +1,16 @@
+from typing import Union
+
+from xchainpy2_mayachain import MRC20_DECIMALS
 from xchainpy2_utils import Amount, Asset
 
 
-def check_ticker(ticker):
+def check_ticker(ticker: Union[Asset, str]):
+    """
+    Check if the MRC20 ticker is valid.
+
+    :param ticker: Asset or str
+    :return: bool
+    """
     if isinstance(ticker, Asset):
         ticker = ticker.ticker
 
@@ -14,10 +23,14 @@ def check_ticker(ticker):
     return ticker
 
 
-MRC20_DECIMALS = 10
-
-
 def get_amount(amount, zero_allowed=False):
+    """
+    Helper function to convert an amount to a string suitable for a memo.
+
+    :param amount: Amount or str
+    :param zero_allowed: if zero amounts are allowed
+    """
+
     amount = Amount.automatic(amount, MRC20_DECIMALS)
     if not zero_allowed and amount.internal_amount <= 0:
         raise ValueError('Must be positive')
@@ -25,8 +38,22 @@ def get_amount(amount, zero_allowed=False):
 
 
 class MRC20Memo:
+    """
+    MRC20Memo is a class that provides methods to generate memos for MRC20 transactions.
+    """
+
     @classmethod
     def deploy_token(cls, ticker, supply, mint_limit=None, mint_price=None) -> str:
+        """
+        Generate a memo for deploying a MRC20 token.
+
+        :param ticker: The ticker of a new token
+        :param supply: Total supply of the token. See: Amount.automatic
+        :param mint_limit: The maximum amount of tokens that can be minted. See: Amount.automatic
+        :param mint_price: The price of minting a token in Cacao. See: Amount.automatic
+        :return: Memo string
+        """
+
         # "MRC-20:deploy:[ticker]:[supply]:[mint-limit]:[mint-price]"
         ticker = check_ticker(ticker)
 
@@ -43,6 +70,14 @@ class MRC20Memo:
 
     @classmethod
     def set_mint_price(cls, ticker, mint_price) -> str:
+        """
+        Generate a memo for setting the mint price of a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param mint_price: The price of minting a token in Cacao. See: AmountInitTypes
+        :return: Memo string
+        """
+
         # "MRC-20:set-mint-price:[ticker]:[mint-price]"
         ticker = check_ticker(ticker)
         mint_price = get_amount(mint_price)
@@ -50,6 +85,15 @@ class MRC20Memo:
 
     @classmethod
     def mint(cls, ticker, amount, recipient=None) -> str:
+        """
+        Generate a memo for minting a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param amount: Amount to mint
+        :param recipient: Recipient of the minted tokens
+        :return: Memo string
+        """
+
         # "MRC-20:mint:[ticker]:[amount]:[recipient]"
         ticker = check_ticker(ticker)
         amount = get_amount(amount)
@@ -61,6 +105,13 @@ class MRC20Memo:
 
     @classmethod
     def transfer(cls, ticker, amount) -> str:
+        """
+        Generate a memo for transferring a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param amount: Amount to transfer
+        :return: Memo string
+        """
         # "MRC-20:transfer:[ticker]:[amount]
         ticker = check_ticker(ticker)
         amount = get_amount(amount)
@@ -69,6 +120,14 @@ class MRC20Memo:
 
     @classmethod
     def sell(cls, ticker, amount, price) -> str:
+        """
+        Generate a memo for placing a sell order for a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param amount: Amount to sell
+        :param price: Price per token in Cacao. See: AmountInitTypes
+        :return: Memo string
+        """
         # "MRC-20:sell:[ticker]:[amount]:[price]"
         ticker = check_ticker(ticker)
         amount = get_amount(amount)
@@ -78,6 +137,13 @@ class MRC20Memo:
 
     @classmethod
     def cancel(cls, ticker, tx_hash) -> str:
+        """
+        Generate a memo for cancelling a MRC20 token sell order.
+
+        :param ticker: MRC20 token ticker
+        :param tx_hash: Transaction hash of the order to cancel
+        :return: Memo string
+        """
         # "MRC-20:cancel:[ticker]:[hash]"
         ticker = check_ticker(ticker)
         if not tx_hash:
@@ -86,6 +152,14 @@ class MRC20Memo:
 
     @classmethod
     def buy(cls, ticker, amount, tx_hash) -> str:
+        """
+        Generate a memo for fulfilling a buy order for a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param amount: Amount to buy
+        :param tx_hash: Transaction hash of the order to fulfill
+        :return: Memo string
+        """
         # "MRC-20:buy:[ticker]:[amount]:[hash]"
         ticker = check_ticker(ticker)
         amount = get_amount(amount)
@@ -97,6 +171,13 @@ class MRC20Memo:
 
     @classmethod
     def burn(cls, ticker, amount) -> str:
+        """
+        Generate a memo for burning a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param amount: Amount to burn
+        :return: Memo string
+        """
         # "MRC-20:burn:[ticker]:[amount]"
         ticker = check_ticker(ticker)
         amount = get_amount(amount)
@@ -105,6 +186,13 @@ class MRC20Memo:
 
     @classmethod
     def stake(cls, ticker, amount) -> str:
+        """
+        Generate a memo for staking a MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :param amount: Amount to stake
+        :return: Memo string
+        """
         # "STAKING:stake:GLD:12340000000000"
         ticker = check_ticker(ticker)
         amount = get_amount(amount)
@@ -112,26 +200,61 @@ class MRC20Memo:
 
     @classmethod
     def withdraw_stake(cls, ticker) -> str:
+        """
+        Generate a memo for withdrawing a staked MRC20 token.
+
+        :param ticker: MRC20 token ticker
+        :return: Memo string
+        """
         # "STAKING:withdraw:GLD
         ticker = check_ticker(ticker)
         return ':'.join(['STAKING', 'withdraw', ticker])
 
     @classmethod
     def claim(cls, ticker) -> str:
+        """
+        Generate a memo for claiming staking rewards.
+
+        :param ticker: MRC20 token ticker
+        :return: Memo string
+        """
         # "STAKING:claim:GLD"
         ticker = check_ticker(ticker)
         return ':'.join(['STAKING', 'claim', ticker])
 
 
 class MNFTMemo:
+    """
+    MNFTMemo is a class that provides methods to generate memos for M-NFT transactions.
+    """
 
     @staticmethod
-    def yes_no(value):
+    def yes_no(value) -> str:
+        """
+        Convert a boolean value to YES or NO string
+
+        :param value: any boolean-like value
+        :return: str
+        """
         return 'YES' if value else 'NO'
 
     @classmethod
     def deploy_token(cls, name, symbol, supply, base_url, pre_minted,
                      minted_by_owner_only=None, mint_price=None) -> str:
+        """
+        Generate a memo for deploying a new M-NFT token.
+        https://docs.mayaprotocol.com/blockchain-explorer/mayascan/m-nfts
+
+        :param name: Name of the token 1-20 characters long
+        :param symbol: Symbol of the token
+        :param supply: Supply of the token 1-100000
+        :param base_url: Base URL for the token metadata
+        :param pre_minted: Whether the token is pre-minted
+        :param minted_by_owner_only: Whether the token can only be minted by the owner
+        :param mint_price: Price of minting a token in Cacao
+        :return: Memo string
+        """
+
         # "M-NFT:deploy:[name]:[symbol]:[supply]:[baseURL]:[preMinted]:[mintedByOwnerOnly]:[mintPrice]"
 
         symbol = check_ticker(symbol)
@@ -156,6 +279,13 @@ class MNFTMemo:
 
     @classmethod
     def update_mint_price(cls, symbol, mint_price) -> str:
+        """
+        Generate a memo for updating the mint price of a M-NFT token.
+
+        :param symbol: M-NFT token symbol
+        :param mint_price: Mint price in Cacao
+        :return: Memo string
+        """
         # "M-NFT:update-mint-price:[symbol]:[mintPrice]"
         symbol = check_ticker(symbol)
         mint_price = get_amount(mint_price)
@@ -163,12 +293,26 @@ class MNFTMemo:
 
     @classmethod
     def set_base_url(cls, symbol, base_url) -> str:
+        """
+        Generate a memo for setting the base URL of a M-NFT token.
+
+        :param symbol: M-NFT token symbol
+        :param base_url: Base URL for the token metadata
+        :return: Memo string
+        """
         # "M-NFT:set-base-url:[symbol]:[baseURL]"
         symbol = check_ticker(symbol)
         return ':'.join(['M-NFT', 'set-base-url', symbol, base_url])
 
     @classmethod
     def mint(cls, symbol, token_id=None) -> str:
+        """
+        Generate a memo for minting a M-NFT token.
+
+        :param symbol: M-NFT token symbol
+        :param token_id: Token identifier to mint
+        :return: Memo string
+        """
         # "M-NFT:mint:[symbol]:[id]"
         symbol = check_ticker(symbol)
         components = ['M-NFT', 'mint', symbol]
@@ -178,12 +322,30 @@ class MNFTMemo:
 
     @classmethod
     def transfer(cls, symbol, token_id) -> str:
+        """
+        Generate a memo for transferring M-NFT token.
+        The token is transferred to the recipient address of the transaction.
+
+        :param symbol: M-NFT token symbol
+        :param token_id: Token identifier to transfer
+        :return: Memo string
+        """
+
         # "M-NFT:transfer:[symbol]:[id]"
         symbol = check_ticker(symbol)
         return ':'.join(['M-NFT', 'transfer', symbol, token_id])
 
     @classmethod
     def sell(cls, symbol, token_id, price) -> str:
+        """
+        Generate a memo for placing a sell order for a M-NFT token.
+
+        :param symbol: M-NFT token symbol
+        :param token_id: Token identifier to sell
+        :param price: Sale price in Cacao
+        :return: Memo string
+        """
+
         # "M-NFT:sell:[symbol]:[id]:[price]"
         symbol = check_ticker(symbol)
         price = get_amount(price)
@@ -191,12 +353,26 @@ class MNFTMemo:
 
     @classmethod
     def cancel(cls, symbol, token_id) -> str:
+        """
+        Generate a memo for cancelling a sell order for a M-NFT token.
+
+        :param symbol: M-NFT token symbol
+        :param token_id: Token identifier to cancel
+        :return: Memo string
+        """
         # "M-NFT:cancel:[symbol]:[id]"
         symbol = check_ticker(symbol)
         return ':'.join(['M-NFT', 'cancel', symbol, token_id])
 
     @classmethod
     def buy(cls, symbol, token_id) -> str:
+        """
+        Generate a memo for fulfilling an order to buy M-NFT token.
+
+        :param symbol: M-NFT token symbol
+        :param token_id: Token identifier to buy
+        :return: Memo string
+        """
         # "M-NFT:buy:[symbol]:[id]"
         symbol = check_ticker(symbol)
         return ':'.join(['M-NFT', 'buy', symbol, token_id])
