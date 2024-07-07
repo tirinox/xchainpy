2e2444cc611f5632e112d7b597346f80c0ea50b9
Tirinox <develoloper@tirinox.ru>
2024-07-07 17:55:48 +0300

    THORNode: fix setup.py desc content type

--- packages/xchainpy_thornode/setup.py
+++ packages/xchainpy_thornode/setup.py
@@ -47,5 +47,6 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description=load_description()
+    long_description=load_description(),
+    long_description_content_type="text/markdown",
 )
