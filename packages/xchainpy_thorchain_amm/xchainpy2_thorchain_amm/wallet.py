fcc48db92f867b264058f98c69bf7fc33841af65
Tirinox <develoloper@tirinox.ru>
2024-07-07 17:37:47 +0300

    AMM: wallet docstrings

--- packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/wallet.py
+++ packages/xchainpy_thorchain_amm/xchainpy2_thorchain_amm/wallet.py
@@ -6,7 +6,7 @@ from typing import Optional, Set, Union
 from xchainpy2_client import NoClient, XChainClient
 from xchainpy2_thorchain_query.models import InboundDetail
 
-from xchainpy2_thorchain_query import THORChainQuery
+from xchainpy2_thorchain_query import THORChainQuery, THORChainCache
 from xchainpy2_utils import Chain, EVM_CHAINS, Asset
 from .models import AllBalances, ChainBalances, ALL
 from .evm_helper import EVMHelper
@@ -15,6 +15,12 @@ from .detect_clients import THORChainClient, CosmosGaiaClient, BinanceSmartChain
 
 
 class Wallet:
+    """
+    Wallet class is a high-level interface to your multi-chain wallet.
+    It allows you to get balances, transaction data, send transactions.
+    You can enable or disable chains according to your needs.
+    """
+
     CLIENT_CLASSES = {
         Chain.THORChain: THORChainClient,
         Chain.Cosmos: CosmosGaiaClient,
@@ -26,12 +32,26 @@ class Wallet:
         Chain.Avalanche: AvalancheClient,
         # to be continued
     }
+    """
+    A dictionary that maps Chain to its client class.
+    """
 
     def __init__(self, phrase: str,
                  query_api: Optional[THORChainQuery] = None,
                  enabled_chains: Set[Chain] = ALL,
                  concurrency: int = 5,
                  default_chain: Chain = Chain.THORChain):
+        """
+        Initialize the Wallet with a mnemonic phrase.
+
+        :param phrase: Seed phrase of the wallet
+        :param query_api: THORChainQuery instance, if not provided, a new instance will be created
+        :param enabled_chains: A set of chains that are enabled for the wallet, if not provided, all chains are enabled
+        :param concurrency: Concurrency level for some async operations
+        :param default_chain: Default chain for the wallet, by default it is THORChain. Used in some methods,
+        like explorer URLs.
+        """
+
         self.default_chain = default_chain
 
         self._semaphore = asyncio.Semaphore(concurrency)
@@ -49,15 +69,40 @@ class Wallet:
         self._init_evm_helpers()
 
     @property
-    def cache(self):
+    def cache(self) -> THORChainCache:
+        """
+        Get the cache instance of the query API.
+
+        :return: THORChainCache
+        """
         return self.query_api.cache
 
     def get_client(self, chain: Union[Chain, Asset]) -> Optional[XChainClient]:
+        """
+        Get the client class instance for the given chain.
+
+        :param chain: Chain or Asset
+        :return: XChainClient or None if not found
+        """
         if isinstance(chain, Asset):
             chain = Chain(chain.chain)
         return self.clients.get(chain)
 
+    def get_thorchain_client(self) -> Optional[THORChainClient]:
+        """
+        Get the THORChain client instance.
+
+        :return: THORChainClient or None if not found
+        """
+        return self.get_client(Chain.THORChain)
+
     def is_chain_enabled(self, chain: Chain):
+        """
+        Check if the given chain is enabled for the wallet.
+
+        :param chain: Chain
+        :return: True if enabled, False otherwise
+        """
         if not self._enabled_chains:
             return True
         return chain in self._enabled_chains
@@ -80,6 +125,11 @@ class Wallet:
             self.clients[chain] = chain_class(phrase=phrase)
 
     async def get_all_balances(self) -> AllBalances:
+        """
+        Get all balances for all enabled chains. Runs concurrently, see the concurrency parameter in the constructor.
+
+        :return: AllBalances
+        """
         result = AllBalances(
             datetime.datetime.now(),
             {}
@@ -115,9 +165,27 @@ class Wallet:
             client.purge_client()
 
     def explorer_url_tx(self, tx_id: str):
+        """
+        Get the explorer URL for the given transaction ID.
+        It calls the get_explorer_tx_url method of the default chain client.
+
+        :param tx_id: Transaction ID or hash
+        :return: URs string
+        """
         cli = self.clients.get(self.default_chain)
+        if not cli:
+            raise ValueError(f"Client for {self.default_chain} is not found.")
         return cli.get_explorer_tx_url(tx_id)
 
     def explorer_url_address(self, address: str):
-        cli: THORChainClient = self.clients.get(self.default_chain)
+        """
+        Get the explorer URL for the given address.
+        It calls the get_explorer_address_url method of the default chain client.
+
+        :param address: Address
+        :return: URL string
+        """
+        cli = self.clients.get(self.default_chain)
+        if not cli:
+            raise ValueError(f"Client for {self.default_chain} is not found.")
         return cli.get_explorer_address_url(address)
