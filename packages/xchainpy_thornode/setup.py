ac1b7099b4a6c70eea9074aae734afe8722004ac
Tirinox <develoloper@tirinox.ru>
2024-09-17 17:08:46 +0300

    THORNode fix setup.py

--- packages/xchainpy_thornode/setup.py
+++ packages/xchainpy_thornode/setup.py
@@ -24,6 +24,20 @@ VERSION = "2.135.0"
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 REQUIRES.append("aiohttp")
 
+
+def load_description():
+    """
+    In setup call you do:
+    long_description=load_description(),
+    long_description_content_type="text/markdown",
+    :return: text
+    """
+    from pathlib import Path
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()
+    return long_description
+
+
 setup(
     name=NAME,
     version=VERSION,
@@ -34,7 +48,5 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Thornode REST API.  # noqa: E501
-    """
+    long_description=load_description(),
 )
