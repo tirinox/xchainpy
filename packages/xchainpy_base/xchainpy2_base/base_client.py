2bd1028acecb1cf5cbc3d99f53009c2da737db76
Tirinox <develoloper@tirinox.ru>
2025-01-11 14:09:31 +0300

    Base: fix explorer url

--- packages/xchainpy_base/xchainpy2_base/base_client.py
+++ packages/xchainpy_base/xchainpy2_base/base_client.py
@@ -1,8 +1,8 @@
 from web3.middleware import geth_poa_middleware
 from web3.providers import BaseProvider
 
-from xchainpy2_avalanche import DEFAULT_AVAX_EXPLORER_PROVIDERS
-from xchainpy2_base import BASE_TOKEN_LIST, BASE_DECIMALS, BASE_CHAIN_ID, FREE_BASE_PROVIDERS
+from xchainpy2_base import BASE_TOKEN_LIST, BASE_DECIMALS, BASE_CHAIN_ID, FREE_BASE_PROVIDERS, \
+    DEFAULT_BASE_EXPLORER_PROVIDERS
 from xchainpy2_ethereum import EthereumClient
 from xchainpy2_ethereum.utils import select_random_free_provider
 from xchainpy2_utils import Chain, AssetBaseETH
@@ -14,7 +14,7 @@ class BaseClient(EthereumClient):
     _GAS_ASSET = AssetBaseETH
     _DECIMAL = BASE_DECIMALS
     _CHAIN_ID_DICT = BASE_CHAIN_ID
-    _EXPLORERS = DEFAULT_AVAX_EXPLORER_PROVIDERS.copy()
+    _EXPLORERS = DEFAULT_BASE_EXPLORER_PROVIDERS.copy()
 
     def _get_default_provider(self):
         return select_random_free_provider(self.network, FREE_BASE_PROVIDERS)
