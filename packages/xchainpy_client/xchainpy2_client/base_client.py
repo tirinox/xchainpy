eec5a82a73ffa5142ed7b1beaa5b8e286ada821f
Tirinox <tirinox@gmail.com>
2025-05-07 19:08:51 +0300

    Client: NetworkType conv

--- packages/xchainpy_client/xchainpy2_client/base_client.py
+++ packages/xchainpy_client/xchainpy2_client/base_client.py
@@ -169,6 +169,10 @@ class XChainClient(abc.ABC):
     def set_network(self, network: NetworkType):
         if not network:
             network = NetworkType.MAINNET
+
+        if not isinstance(network, NetworkType):
+            network = NetworkType(network)
+
         self.network = network
 
         # Fire off a warning in the console to indicate that stagenet and real assets are being used.
