e3514465a1e4ae1b3c05d3eb7db2e81dcfeaffbf
Tirinox <tirinox@gmail.com>
2025-07-05 16:30:18 +0300

    Doge: fix __init__ args

--- packages/xchainpy_dogecoin/xchainpy2_dogecoin/doge_client.py
+++ packages/xchainpy_dogecoin/xchainpy2_dogecoin/doge_client.py
@@ -3,10 +3,10 @@ from typing import Optional, Union
 from bitcoinlib.services.dogecoind import DogecoindClient
 
 from xchainpy2_bitcoin import BitcoinClient
-from xchainpy2_client import FeeBounds, RootDerivationPaths
+from xchainpy2_client import RootDerivationPaths
 from xchainpy2_utils import NetworkType, Asset, Chain, AssetDOGE
 from .const import ROOT_DERIVATION_PATHS, DOGE_DECIMAL, DEFAULT_PROVIDER_NAMES, DEFAULT_DOGE_EXPLORERS, \
-    DOGE_DEFAULT_FEE_BOUNDS, AssetTestDOGE
+    AssetTestDOGE
 
 
 class DogecoinClient(BitcoinClient):
@@ -14,20 +14,17 @@ class DogecoinClient(BitcoinClient):
                  network=NetworkType.MAINNET,
                  phrase: Optional[str] = None,
                  private_key: Union[str, bytes, callable, None] = None,
-                 fee_bound: Optional[FeeBounds] = DOGE_DEFAULT_FEE_BOUNDS,
                  root_derivation_paths: Optional[RootDerivationPaths] = ROOT_DERIVATION_PATHS,
                  explorer_providers=DEFAULT_DOGE_EXPLORERS,
                  wallet_index=0,
                  provider_names=DEFAULT_PROVIDER_NAMES,
                  daemon_url: Optional[str] = None):
         """
-        DogecoinClient interface
-        Constructor to create a new Do.
+        Constructor to create a new DogecoinClient.
 
         :param network: The network type
         :param phrase: The seed phrase
         :param private_key: The private key
-        :param fee_bound: The fee bound
         :param root_derivation_paths: The root derivation paths
         :param explorer_providers: The explorer providers
         :param wallet_index: The wallet index (default is 0)
@@ -37,7 +34,7 @@ class DogecoinClient(BitcoinClient):
         if not provider_names:
             provider_names = DEFAULT_PROVIDER_NAMES
 
-        super().__init__(network, phrase, private_key, fee_bound, root_derivation_paths, explorer_providers,
+        super().__init__(network, phrase, private_key, root_derivation_paths, explorer_providers,
                          wallet_index, provider_names, daemon_url,
                          _chain=Chain.Doge)
 
